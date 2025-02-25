825a875dabe634322b685ef4943cc7ae985f4811
juguzmanp <juguzmanp@unal.edu.co>
2025-02-25 16:20:06 -0500

    Add files via upload

---
+++ sockets/src/socket/control.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socket;
+
+/**
+ *
+ * @author locon
+ */
+public class control {
+    
+    public static void main(String[] args) {
+        
+        socket1 cliente = new socket1();
+        socket2 server = new socket2();
+
+    }
+    
+}
